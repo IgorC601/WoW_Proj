10d3145c7ce8db717039cf2ac5416180e6e0d0fa
IgorC601 <igorccaetano601@gmail.com>
2024-07-19 12:51:03 +0100

    add Authentication

--- bot/top_k.py
+++ bot/top_k.py
@@ -8,6 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+import hmac
 
 ###################################################################
 
@@ -52,6 +53,31 @@ def connect_to_pinecone():
     return pc.Index('wordsofwisdom')
 
 ###################################################################
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
 
 def run_chatbot_app():
     # Initialize Pinecone
@@ -120,5 +146,9 @@ def run_chatbot_app():
                 paragraph = doc.metadata['paragraph']
                 with st.expander(f"{title} - {paragraph}"):
                     st.markdown(content)
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 run_chatbot_app()
