f5c111a26ccb7396c3681eb57ec27ddeb85e281c
IgorC601 <igorccaetano601@gmail.com>
2024-07-19 15:51:13 +0100

    add new API call

--- bot/top_k.py
+++ bot/top_k.py
@@ -14,9 +14,10 @@ from dotenv import load_dotenv
 
 
 ###################################################################
-load_dotenv()
+#load_dotenv()
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+#OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 embed = OpenAIEmbeddings(
    model = 'text-embedding-ada-002',
@@ -51,7 +52,9 @@ class StreamHandler(BaseCallbackHandler):
 
 ###################################################################
 
-PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+#PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_API_KEY = st.secrets["OPENAI_API_KEY"]
+
 
 @st.cache_resource
 def connect_to_pinecone():
